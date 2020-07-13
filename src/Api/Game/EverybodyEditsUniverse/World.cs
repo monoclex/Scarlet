a5f75ee38a23068dd96e207c06172b62431e7548
SirJosh3917 <SirJosh3917@users.noreply.github.com>
2020-07-13 00:38:51 -0400

    lowercase property names for EEU world api

--- src/Api/Game/EverybodyEditsUniverse/World.cs
+++ src/Api/Game/EverybodyEditsUniverse/World.cs
@@ -4,11 +4,22 @@ namespace Scarlet.Api.Game.EverybodyEditsUniverse
 {
 	public class World
 	{
+		[JsonPropertyName("plays")]
 		public int Plays { get; set; }
+
+		[JsonPropertyName("owner")]
 		public string Owner { get; set; }
+
+		[JsonPropertyName("backgroundColor")]
 		public int BackgroundColor { get; set; }
+
+		[JsonPropertyName("width")]
 		public int Width { get; set; }
+
+		[JsonPropertyName("height")]
 		public int Height { get; set; }
+
+		[JsonPropertyName("name")]
 		public string Name { get; set; }
 
 		[JsonIgnore]
